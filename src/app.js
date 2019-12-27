e14418a86d4a6a716b2530ef08001a166cf82c7a
Alex Langshur <adlangshur@gmail.com>
2019-12-26 23:19:59 -0500

    Removed extraneous dev dependencies

--- src/app.js
+++ src/app.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+
 
 class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.instructions}>Hello world!</Text>
+				<Text style={styles.instructions}>App</Text>
 			</View>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF'
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5
-	}
-});
 
 export default App;
